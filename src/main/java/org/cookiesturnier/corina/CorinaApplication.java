ad359df99e19bfc7f15ab750b61da6674269a19c
Lenny Angst <lenny.angst@easyid.ch>
2021-07-10 22:22:05 +0200

    Initial commit

---
+++ src/main/java/org/cookiesturnier/corina/CorinaApplication.java
@@ -0,0 +1,13 @@
+package org.cookiesturnier.corina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cookiesturnier/corina/CorinaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cookiesturnier.corina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
