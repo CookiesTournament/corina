015c14e3e19daa75cbdaa03eb2a78d162ca05bc9
Lenny Angst <lenny.angst@easyid.ch>
2021-07-11 12:01:21 +0200

    Add dev and prod thingies

--- src/main/java/org/cookiesturnier/corina/repositories/PlayerRepo.java
+++ src/main/java/org/cookiesturnier/corina/repositories/PlayerRepo.java
@@ -2,11 +2,12 @@ package org.cookiesturnier.corina.repositories;
 
 import org.cookiesturnier.corina.entities.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PlayerRepo extends JpaRepository<Player, UUID> {
 
     Optional<Player> getByUsername(String username);
