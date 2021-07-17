ac1c21f0a3506b4419ef553678341bd437794769
VoxCrafterLP <58361830+VoxCrafterLP@users.noreply.github.com>
2021-07-18 01:19:48 +0200

    Add unit test

--- src/test/java/org/cookiesturnier/corina/services/MapRouletteServiceTest.java
+++ src/test/java/org/cookiesturnier/corina/services/MapRouletteServiceTest.java
@@ -4,12 +4,6 @@ import org.cookiesturnier.corina.enums.Gamemode;
 import org.cookiesturnier.corina.enums.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +18,7 @@ public class MapRouletteServiceTest {
     @Test
     @DisplayName("Test with duo cookies map")
     public void testCookiesDuo() {
-        Map result = mapRouletteService.getRandomMap(Gamemode.COOKIES, false, 2);
+        final Map result = mapRouletteService.getRandomMap(Gamemode.COOKIES, false, 2);
         assertEquals(Gamemode.COOKIES, result.getGamemode(), "Gamemode wrong");
         assertEquals(2, result.getTeamSize(), "Team size is wrong");
     }
@@ -32,10 +26,17 @@ public class MapRouletteServiceTest {
     @Test
     @DisplayName("Test with duo or squad public cookies map")
     public void testCookiesDuoSquadPublic() {
-        Map result = mapRouletteService.getRandomMap(Gamemode.COOKIES, true, 2, 4);
+        final Map result = mapRouletteService.getRandomMap(Gamemode.COOKIES, true, 2, 4);
         assertEquals(Gamemode.COOKIES, result.getGamemode(), "Gamemode wrong");
         assertTrue(result.getTeamSize() == 2 || result.getTeamSize() == 4, "Team size is wrong");
         assertTrue(result.isPublic(), "Map is not public");
+    }
+
+    @Test
+    @DisplayName("Test with trio cookies map")
+    public void testCookiesTrio() {
+        final Map result = mapRouletteService.getRandomMap(Gamemode.COOKIES, false, 3);
+        assertNull(result, "Map is not null");
     }
 
 }
